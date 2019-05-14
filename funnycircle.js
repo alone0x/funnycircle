0ae6a1d385954752dd2c7bd3127a016069638efc
alone0x <49860410+alone0x@users.noreply.github.com>
2019-05-14 00:18:26 -0700

    nộp bài lần 1

---
+++ funnycircle.js
@@ -0,0 +1,34 @@
+function Circle(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+}
+
+function creatCircle() {
+    let ctx = document.getElementById("canvas").getContext("2d");
+    let radius = Math.floor(Math.random() * 80);
+    let x = getRandomhex();
+    let y = getRandomhex();
+    let circle = new Circle(x, y, radius);
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+function getRandomhex() {
+    return Math.floor(Math.random() * 500)
+}
+
+function run() {
+    ctx.clearRect();
+    x += 3;
+    y += 5;
+}
+
+function multiCircle(x) {
+    for (i = 1; i <= x; i++) {
+        creatCircle();
+    }
+}
+
+multiCircle(10);
